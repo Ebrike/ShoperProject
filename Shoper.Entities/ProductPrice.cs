d70c56e56787193d5c49df103062903c32c81ef4
Ebrike <karacaebrike@gmail.com>
2022-12-10 07:00:03 +0300

    Management eklendi

--- Shoper.Entities/ProductPrice.cs
+++ Shoper.Entities/ProductPrice.cs
@@ -1,19 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace Shoper.Entities
 {
     public class ProductPrice
     {
+        [Display(Name = "Fiyat No")]
         public int PriceId { get; set; }
+
+        [Display(Name = "Fiyat Birimi")]
         public UnitPrice UnitPrice { get; set; }
 
+        [Display(Name = "Ürün Kodu ")]
         public int ProductId { get; set; } // hangi ürüne ait olduğunu belli etmek için
-                                          //foreign key 
+                                           //foreign key 
+        [Display(Name = "Ürün Fiyatı ")]
         public decimal Price { get; set; }
+        [Display(Name = "Geçerli Fiyat")]
         public bool isValidFlag { get; set; } // ürünün fiyatının geçerli olup olmadığı
 
         public Product Product { get; set; } // bu fiyat bilgisi bir ürüne ait 
@@ -23,8 +31,9 @@ namespace Shoper.Entities
     }
     public enum UnitPrice
     {
-        Dolar,
+        
         Lira,
+        Dolar,
         Euro
     }
 }
